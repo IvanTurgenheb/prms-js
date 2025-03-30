434e1bf2ff048c9c910b2a1651f5629d29e643c6
lsj0202 <w1004123@gmail.com>
2025-03-30 21:02:57 +0900

    가계부 구현하기

--- js/components/history-list.js
+++ js/components/history-list.js
@@ -14,8 +14,9 @@ function addHistoryListEventListener() {
     if (!element.className.includes("delete-button")) return;
 
     const { dateid, itemid } = element.dataset;
-
+    console.log(dateid, itemid);
     const isSuccess = removeHistory(dateid, itemid);
+    console.log(isSuccess);
     if (!isSuccess) {
       alert("소비내역 삭제에 실패했습니다.");
       return;
@@ -31,45 +32,51 @@ function reRender() {
 }
 
 export function renderHistoryList() {
-  // TODO: 데이터 매핑
-  // TODO: 오름차순으로 목록 나열
-  // TODO: 항목의 시간 포맷 변경: `HH:mm`
-  // TODO: 금액 콤마 포맷 맞추기
-
   $sectionHistory.innerHTML = store.dateList
-    .map(({ date, id: dateId }) => {
+    .map(({ id: dateId }) => {
       const detail = store.detailList[dateId];
       if (!detail?.length) return "";
 
       return `<article class="history-per-day">
       <p class="history-date">2021년 12월 1일</p>
-      <section class="history-item">
-        <section class="history-item-column">
-          <div class="create-at">10:30</div>
-          <div class="history-detail">
-            <div class="history-detail-row history-detail-title">
-              <p>아이스 아메리카노</p>
-            </div>
-            <div class="history-detail-row history-detail-subtitle">
-              <p>카페</p>
+      ${detail
+        .sort((a, b) => b.id - a.id)
+        .map(
+          ({ description, category, amount, fundsAtTheTime, createAt, id }) => {
+            const time = new Date(createAt).toLocaleTimeString("ko-kr", {
+              timeStyle: "short",
+              hourCycle: "h24",
+            });
+            return `<section class="history-item">
+            <section class="history-item-column">
+              <div class="create-at">${time}</div>
+              <div class="history-detail">
+                <div class="history-detail-row history-detail-title">
+                  <p>${description}</p>
+                </div>
+                <div class="history-detail-row history-detail-subtitle">
+                  <p>${category}</p>
+                  <p>
+                    ${amount.toLocaleString()}
+                    <span>원</span>
+                  </p>
+                </div>
+              </div>
+              <div class="delete-section">
+                <button class="delete-button" data-dateid="${dateId}" data-itemid="${id}">🗑</button>
+              </div>
+            </section>
+            <section class="history-item-caption">
               <p>
-                1000000
+                <span>남은 자산</span>
+                <span>${fundsAtTheTime.toLocaleString()}</span>
                 <span>원</span>
               </p>
-            </div>
-          </div>
-          <div class="delete-section">
-            <button class="delete-button">🗑</button>
-          </div>
-        </section>
-        <section class="history-item-caption">
-          <p>
-            <span>남은 자산</span>
-            <span>300000</span>
-            <span>원</span>
-          </p>
-        </section>
-      </section>
+            </section>
+        </section>`;
+          }
+        )
+        .join("")}
     </article>`;
     })
     .join("");
